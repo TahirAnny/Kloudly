bd6bdcf32b47b587dfeb467b69540caeb1c1dc4d
TahirAnny <tahiraanny@gmail.com>
2025-04-15 13:31:28 -0400

    theme toggle added

--- Kloudly/src/components/header.tsx
+++ Kloudly/src/components/header.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { ModeToggle } from './mode-toggle'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+            <Link to="/">
+                Klimate
+            </Link>
+
+            <div>
+                <ModeToggle />
+            </div>
+        </div>
+    </header>
   )
 }
 
